edf944e4754e7c8a6a3c3c15ffd274a7b6fb2c2c
Alonzo <alonzomarquez65@gmail.com>
2024-03-03 22:14:00 -0700

    init files

---
+++ backend/main.py
@@ -0,0 +1,103 @@
+# uvicorn main:app
+# uvicorn main:app --reload
+
+#Main Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config 
+import openai
+
+# Custom Function Imports
+from functions.database import store_messages, reset_messages
+from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.text_to_speech import convert_text_to_speech
+
+
+#initiate App
+app = FastAPI()
+
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Check Health
+@app.get("/health")
+async def check_health():
+    
+    return{"message": "Healthy"}
+
+#Reset messages in json file database
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return{"message": "conversation reset"}
+
+
+# Get audio
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+    
+    # Get saved audio
+    #audio_input = open("voicetest.mp3", "rb")
+    
+    #Save file from Frontend
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+        
+    
+    # Decode Audio
+    message_Decoded = convert_audio_to_text(audio_input)
+    
+    #Guard: Ensure message decoded
+    if not message_Decoded:
+        return HTTPException(status_code = 400, detail = "Failed to decode audio")
+    
+    #Get ChatGPT Response
+    chat_response = get_chat_response(message_Decoded)
+    
+    #Guard: Ensure response
+    if not chat_response:
+        return HTTPException(status_code = 400, detail = "Failed to get chat response")
+    
+    
+    #Store messages
+    store_messages(message_Decoded, chat_response)
+    
+    
+    #Convert chat response to audio
+    audio_output = convert_text_to_speech(chat_response)
+    
+    #Guard: Ensure we get eleven labs audio
+    if not audio_output:
+        return HTTPException(status_code = 400, detail = "Failed to get Eleven Labs audio response")
+    
+    #Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+        
+    #Return audio file
+    #return StreamingResponse(iterfile(), media_type="audio/mpeg")
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
+    
+
+
+
+
