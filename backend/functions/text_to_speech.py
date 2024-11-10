01b04f5933278f308b9eaba45a26a3bc1ebaf6d5
Alonzo <59700500+Alonzo65@users.noreply.github.com>
2024-11-10 14:37:49 -0700

    Update text_to_speech.py

--- backend/functions/text_to_speech.py
+++ backend/functions/text_to_speech.py
@@ -16,11 +16,11 @@ def convert_text_to_speech(message):
     }
     
     #Define voice
-    voice_rachel = "21m00Tcm4TlvDq8ikWAM"
+    voice_casca = "XB0fDUnXU5powFXDhCwa"
     
     #Construct Headers and Endpoint
     headers = {"xi-api-key": ELEVEN_LABS_API_KEY, "Content-Type": "application/json", "accept": "audio/mpeg"}
-    endpoint = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_rachel}"
+    endpoint = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_casca}"
     
     #Send request
     try:
